9586cd0ee5a679686fa2eb3dcc6fc233309dfc3a
Kevin G <kgamboa92@yahoo.com>
2016-11-29 13:35:37 -0800

    Add files via upload

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,5 +1,17 @@
-var myApp = angular.module('myApp',[]);
-myApp.controller('AppCtrl', ['$scope', '$http',
-	function($scope, $http){
-		console.log("Hellow World from controller");
-	}]);
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log("port 8080 default");
+    $scope.content = 'first';
+
+
+var refresh = function() {
+  $http.get('/resume').success(function(response) {
+    console.log("I got the data I requested");
+    $scope.resume = response; 
+  });
+};
+
+refresh();
+
+
+}]);﻿
--- server.js
+++ server.js
@@ -1,19 +1,56 @@
-/*var http = require('http');
 
-http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type':'text/plain'});
-	res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-
-console.log('Server running at http://127.0.1:1337/');
-*/
-
-//copied code from class
-
-var express = require("express");
+var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('resume', ['resume']);
+var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-app.listen(3000);
-console.log("Server running on port 3000");
+app.get('/resume', function (req, res) {
+  console.log('I received a GET request');
+
+  db.resume.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post('/resume', function (req, res) {
+  console.log(req.body);
+  db.resume.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/resume/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.resume.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/resume/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.resume.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/resume/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.school);
+  db.resume.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {school: req.body.school, degree: req.body.degree, lastyear: req.body.lastyear}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+
+app.listen(8080);
+console.log("Server running on port 8080");
